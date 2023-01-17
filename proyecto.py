6d76190d5c9d463b7d14f6bbb754a4ba78f4408d
Julian <al_jdomenecht@iessierradelasvillas.org>
2023-01-17 14:08:46 +0100

    Update

--- proyecto.py
+++ proyecto.py
@@ -3,12 +3,16 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "DropdownMenu Equipos"
 
+
+
+
     Dropdown_Equipos=ft.Dropdown(width=300,options=[ft.dropdown.Option("Rayo Fuentealbilla")],label="Equipos")
     Dropdown_Equipos.options.append(ft.dropdown.Option("Depor"))
     Dropdown_Equipos.options.append(ft.dropdown.Option("Leganés"))
     Dropdown_Equipos.options.append(ft.dropdown.Option("Real Zaragoza"))
     Dropdown_Equipos.options.append(ft.dropdown.Option("Borusia Mochenglasbach"))
     page.add (Dropdown_Equipos)
+
 
 
 ft.app(target=main)
