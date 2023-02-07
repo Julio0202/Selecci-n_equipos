6e0a1015e0111da634dd0f70242dc0dc21f2f0ce
Julio Chen <al_ychen@iessierradelasvillas.org>
2023-02-07 14:37:08 +0100

    Haciendo el proyecto

--- proyecto.py
+++ proyecto.py
@@ -3,10 +3,10 @@
 import flet as ft
 
 def main(page: ft.Page):
+    lv = ft.ListView(expand=1,spacing=5, padding=20, auto_scroll=True)
     page.title = "DropdownMenu Equipos"
     imag = ""
     def dropdown_changed(e):
-        page.update()
         if (menu.value == "Rayo Fuentealbilla"):
             imag = "RAYO_FUENTEALBILLA.jpg"
         elif (menu.value == "Depor"):
@@ -18,32 +18,30 @@ def main(page: ft.Page):
         else:
             (menu.value=="Borusia")
             imag = "BORUSIA_MONCHENGLASBACH.png"
-
         img.src=f"{imag}"
         page.update()
-    
+        
     def equipolista(e):
         seleccionado=""
-        
         if(vEquiposSelecionados.count(menu.value)==0):
             vEquiposSelecionados.append(menu.value)
             print(vEquiposSelecionados)
-        
+            lv.controls.append(vEquiposSelecionados)
         else:
             dlg = ft.AlertDialog(title=ft.Text("Equipo ya seleccionado"))
             page.dialog = dlg
             dlg.open = True
             page.update()
+
         
 
     def cargarequipos():
         vEquipos=[]
-        f= open("Equipos.txt","r")
-        for linea in f:
-            vEquipos.append(linea) 
+        f=open("Equipos.txt","r")
+        linea = f.readline()
+        vEquipos = linea.split(sep=";")
         f.close()
         return vEquipos
-        
 
 
 
@@ -57,18 +55,14 @@ def main(page: ft.Page):
             height=300
         )
 
-    vEquipos = cargarequipos()
     vEquiposSelecionados = []
-    
-    
-
-
+    vEquipos = cargarequipos()
     for equipo in vEquipos:
         menu.options.append(ft.dropdown.Option(equipo))
     
     boton=ft.ElevatedButton(text="Añadir equipo",on_click=equipolista)
     
-    
-    page.add(menu,img)
+
+    page.add(menu,img,lv)
     page.add(boton)
 ft.app(target=main,assets_dir="Imagenes")
